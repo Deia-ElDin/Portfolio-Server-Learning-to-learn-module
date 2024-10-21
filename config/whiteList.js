12d29ce01c3934e672d845d3fd44c93eac69bf71
Deia Hamad <deia.tech2021@gmail.com>
2024-10-21 12:49:31 +0400

    updated urls

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,3 +1,7 @@
-const whiteList = ["https://deia-eldin.github.io", "http://localhost:3000"];
+const whiteList = [
+  "https://deia-eldin.github.io",
+  "http://localhost:3000",
+  "https://deia-portfolio.vercel.app",
+];
 
 module.exports = whiteList;
